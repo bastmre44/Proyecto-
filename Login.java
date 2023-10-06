5c3e2ad33ecca6405f71393e4eb776af21b12dc7
bastmre44 <134813711+bastmre44@users.noreply.github.com>
2023-10-06 13:10:49 -0600

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,9 +1,1 @@
-import com.opencsv.CSVReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
 
-public class Login {
-    private String archivo = "usuarios.csv";
-
-}
