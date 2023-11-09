baa854ffb61e0895eb195456a03693004c5833e9
bastmre44 <134813711+bastmre44@users.noreply.github.com>
2023-11-08 23:39:57 -0600

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class Menu {
     public static void main(String[] args) {
-        Scanner leer = new Scanner(System.in);
+        Scanner leer = new Scanner(System.in); //objeto scanner 
         System.out.println("Bienvenido al sistema de Swift Vision, por favor selecciona una opción ");
         System.out.println("1. Iniciar Sesión");
         System.out.println("2. Registrarse");
         System.out.println("3. Salir");
-        int opcion = Integer.parseInt(leer.nextLine());
+        int opcion = Integer.parseInt(leer.nextLine()); // Leer la opción ingresada por el usuario
+
 
         if (opcion == 1) {
             Login.main(args);
