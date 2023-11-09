7c5c5e7317010211a1c557cc9bc15ff7c4da62da
bastmre44 <134813711+bastmre44@users.noreply.github.com>
2023-11-08 18:43:41 -0600

    Update historialclic.java
    clase para historial clinico para encontrar los registros 
    falta agregar para escrbir en los registros

--- historialclic.java
+++ historialclic.java
@@ -1,3 +1,55 @@
-public class historialclinico {
-    
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class historialclic {
+    private String archivoClientes = "clientes.csv";
+
+    public void mostrarHistorialPorCliente(String nombreCliente) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(archivoClientes));
+            String linea;
+            int contador = 0;
+
+            while ((linea = br.readLine()) != null) {
+                String[] datos = linea.split(",");
+                String usuario = datos[0].trim(); // El índice 0 corresponde a la columna "Usuario" 
+
+                if (usuario.equalsIgnoreCase(nombreCliente)) {
+                    // Mostrar todos los datos del registro del historial clínico.
+                    System.out.println("Usuario: " + datos[0]);
+                    System.out.println("Fecha: " + datos[1]);
+                    System.out.println("Hora: " + datos[2]);
+                    System.out.println("Diagnóstico: " + datos[3]);
+                    System.out.println("Doctor: " + datos[4]);
+                    System.out.println("Farmacia: " + datos[5]);
+                    System.out.println("Total: " + datos[6]);
+                    System.out.println("------------");
+
+                    contador++;
+                }
+            }
+
+            br.close();
+
+            if (contador == 0) {
+                System.out.println("No se encontraron registros para el cliente " + nombreCliente);
+            } else {
+                System.out.println("Total de registros encontrados: " + contador);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        historialclic historial = new historialclic();
+
+        if (args.length > 0) {
+            historial.mostrarHistorialPorCliente(args[0]);
+        } else {
+            System.out.println("Por favor, ingresa el nombre de cliente como argumento.");
+        }
+    }
 }
