505f755844817c62191420ca91fc3db82debbe9d
Yee404 <yee231193@uvg.edu.gt>
2023-11-08 23:42:17 -0600

    Commit5
    Guardar datos del usuario

--- Citas.java
+++ Citas.java
@@ -11,5 +11,36 @@ public class Citas extends Consulta {
             int total) {
         super(usuario, fecha, diagnostico, doctor, farmacia, hora, total);
     }
+// Commit 5 (guardar datos del usuario)
+    public void guardarcitas(Consulta consulta) {
+
+        try {
+            FileWriter insertar = new FileWriter(Archivo, true);
+            insertar.append(consulta.getUsuario());
+            insertar.append(",");
+
+            insertar.append(consulta.getFecha());
+            insertar.append(",");
+
+            insertar.append(consulta.getDiagn());
+            insertar.append(",");
+
+            insertar.append(consulta.getDoctor());
+            insertar.append(",");
+
+            insertar.append(consulta.getFarmacia());
+            insertar.append(",");
+
+            insertar.append(consulta.getHora());
+            insertar.append(",");
+
+            insertar.append(consulta.getTotal());
+            insertar.append(",");
+        } catch (IOException e) {
+            System.err.println("Error, no se guardaron los datos: " + e.getMessage());
+        }
+    }
 
     
+
+}
