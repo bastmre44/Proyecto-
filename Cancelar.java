91362685800cf12c7c74b37427a88e596b2a30ac
bastmre44 <134813711+bastmre44@users.noreply.github.com>
2023-11-22 13:02:10 -0600

    Update Cancelar.java
    se edito la clase

--- Cancelar.java
+++ Cancelar.java
@@ -1,45 +1,69 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class Cancelar {
+    private String archivo = "clientes.csv";
+    //Método para cancelar una cita
+    public Cancelar() {
+        // Constructor 
+    }
 
-    public static void main(String[] args) {
-        String temporal = "temporal.csv";
-        String original = "clientes.csv";
-        String linea = "";
-        String nombre = "";
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese el nombre de la persona con la que se guardó la cita:");
-        nombre = scanner.nextLine();
-
+    public void cancelarCita(LocalDate fecha, int hora) {
         try {
-            BufferedReader read = new BufferedReader(new FileReader(original));
-            BufferedWriter write = new BufferedWriter(new FileWriter(temporal));
+            BufferedReader br = new BufferedReader(new FileReader(archivo));
+            StringBuilder datos = new StringBuilder();
+            String linea;
 
-            while ((linea = read.readLine()) != null) {
-                if (!linea.contains(nombre)) {
-                    write.write(linea);
-                    write.newLine();
+            while ((linea = br.readLine()) != null) {
+                if (linea.startsWith("Usuario, Fecha, Hora, Diagnostico, Doctor, Farmacia, Total")) {
+                    datos.append(linea).append("\n");
+                    continue;
+                }
+
+                String[] columnas = linea.split(",");
+
+                try {
+                    LocalDate fechaCita = LocalDate.parse(columnas[1].trim());
+                    int horaCita = Integer.parseInt(columnas[2].trim());
+
+                    if (!(fechaCita.equals(fecha) && horaCita == hora)) {
+                        datos.append(linea).append("\n");
+                    }
+                } catch (DateTimeParseException e) {
+                    System.err.println("Error al procesar la fecha: " + e.getMessage());
                 }
             }
 
-            read.close();
-            write.close();
-            scanner.close();
+            br.close();
 
-            // Eliminamos el archivo original
-            File antiguo = new File(original);
-            antiguo.delete();
+            FileWriter escribir = new FileWriter(archivo);
+            escribir.write(datos.toString());
+            escribir.close();
 
-            // Renombramos el nuevo archivo (temporal)
-            File nuevo = new File(temporal);
-            nuevo.renameTo(antiguo);
+            System.out.println("La cita ha sido cancelada exitosamente.");
+        } catch (IOException e) {
+            System.err.println("Error al cancelar la cita: " + e.getMessage());
         }
+    }
 
-        catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
 
+        System.out.print("Ingrese la fecha de la cita a cancelar (AAAA-MM-DD): ");
+        String fechaStr = leer.nextLine();
+        LocalDate fecha = LocalDate.parse(fechaStr);
+
+        System.out.print("Ingrese la hora de la cita a cancelar: ");
+        int hora = Integer.parseInt(leer.nextLine());
+
+        leer.close();
+        // instancia de la clase CancelarCita y cancelar la cita
+        Cancelar cancelador = new Cancelar();
+        cancelador.cancelarCita(fecha, hora);
     }
 }
